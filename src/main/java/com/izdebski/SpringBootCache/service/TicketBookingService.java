dda53b73e1475469ea7e6c7db77e255f9105abcd
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2018-12-24 19:37:33 +0200

    Spring boot-Guava Cache Integration

---
+++ src/main/java/com/izdebski/SpringBootCache/service/TicketBookingService.java
@@ -0,0 +1,34 @@
+package com.izdebski.SpringBootCache.service;
+
+import com.izdebski.SpringBootCache.dao.TicketBookingDao;
+import com.izdebski.SpringBootCache.entities.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketBookingService {
+
+    @Autowired
+    private TicketBookingDao ticketBookingDao;
+
+    @Cacheable(value="ticketsCache",key="#ticketId", unless = "#result==null")
+    public Ticket getTicketById(Integer ticketId){
+        return ticketBookingDao.findOne(ticketId);
+    }
+
+    @CacheEvict(value="ticketsCache",key="#ticketId")
+    public void deleteTicket(Integer ticketId) {
+        ticketBookingDao.delete(ticketId);
+    }
+
+    @CachePut(value="ticketsCache",key="#ticketId")
+    public Ticket updateTicket(Integer ticketId, String newEmail) {
+        Ticket ticketFromDb = ticketBookingDao.findOne(ticketId);
+        ticketFromDb.setEmail(newEmail);
+        Ticket upatedTicket = ticketBookingDao.save(ticketFromDb);
+        return upatedTicket;
+    }
+}
